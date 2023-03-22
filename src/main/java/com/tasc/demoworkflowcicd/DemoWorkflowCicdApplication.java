f1032cb274f07841a5b259d64a7016826b6b05d0
nghiaha1 <maxx.minimizee@gmail.com>
2023-03-22 08:32:11 +0700

    code

---
+++ src/main/java/com/tasc/demoworkflowcicd/DemoWorkflowCicdApplication.java
@@ -0,0 +1,13 @@
+package com.tasc.demoworkflowcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWorkflowCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWorkflowCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tasc/demoworkflowcicd/DemoWorkflowCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasc.demoworkflowcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWorkflowCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
